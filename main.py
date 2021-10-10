4f33f9427ab61ca8e4b998c7d0a8fc7d0f699add
Darko Novak <novak.darko1@gmail.com>
2021-10-10 11:30:25 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+mood = "happy"
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Talk to me.")
+elif mood == "excited":
+    print("Yeaaahh!")
+elif mood == "relaxed":
+    print ("You are calm and not worried or tense.")
+else:
+    print("I don't recognize this mood.")
